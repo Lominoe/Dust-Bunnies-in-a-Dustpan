0305d88514cf5b99b530d98e7db091f47d5457ae
IvTwo <113319454+ivu-u@users.noreply.github.com>
2026-01-09 22:53:29 -0500

    Basic Player Movement
    - WASD to move
    - Mouse to turn

--- 2026-Pitch-Proj/Assets/_Scripts/PlayerMovement.cs
+++ 2026-Pitch-Proj/Assets/_Scripts/PlayerMovement.cs
@@ -3,17 +3,47 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private CharacterController characterController;
+    [SerializeField] private Transform camHead;
     [SerializeField] private float speed = 10f;
-    [SerializeField] private float rotationSpeed = 180f;
+    [SerializeField] private float rotationSpeed = 2f;
+
+    void Start() {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     // Update is called once per frame
-    void Update()
-    {
-        Vector3 rotation = new Vector3(0, Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime, 0);
+    void Update() {
+        Vector3 move = new(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
-        Vector3 move = new Vector3(0, 0, Input.GetAxisRaw("Vertical") * Time.deltaTime);
         move = transform.TransformDirection(move);
-        characterController.Move(move * speed);
-        transform.Rotate(rotation);
+        move.Normalize();
+        characterController.SimpleMove(move * speed);
+
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * rotationSpeed);
+    }
+
+    void LateUpdate() {
+        Vector3 e = camHead.eulerAngles;
+        e.x -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        e.x = RestrictAngle(e.x, -85f, 85f);
+        camHead.eulerAngles = e;
+    }
+
+    /// <summary>
+    /// Helper method for head rotation, to prevent bending backwards
+    /// </summary>
+    private float RestrictAngle(float angle, float min, float max) {
+        if (angle > 180)
+            angle -= 360;
+        else if (angle < -180)
+            angle += 360;
+
+        if (angle > max)
+            angle = max;
+        if (angle < min)
+            angle = min;
+
+        return angle;
     }
 }
